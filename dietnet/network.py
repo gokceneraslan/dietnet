0deb764a26d150414c9df1096627debaa682e672
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2017-06-18 21:16:12 +0200

    Reimplement in Keras.

--- dietnet/network.py
+++ dietnet/network.py
@@ -14,129 +14,115 @@
 # ==============================================================================
 
 import numpy as np
-import tensorflow as tf
-slim = tf.contrib.slim
+from keras.layers import Input, Dense, Dropout, Lambda, Activation, BatchNormalization
+from keras.models import Model
+from keras import backend as K
+from keras.engine.topology import Layer
 
 
-def embedding(inputs, size, dropout_rate=0.5, is_training=True, reuse=None, scope=None):
-    with slim.arg_scope([slim.fully_connected],
-                        weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                        weights_regularizer=slim.l2_regularizer(0.005),
-                        activation_fn=tf.nn.relu):
+class Bias(Layer):
+    def __init__(self, output_dim, **kwargs):
+        self.output_dim = output_dim
+        super().__init__(**kwargs)
 
-        # Embedding layer, can be shared depending on the reuse parameter
-        net = slim.fully_connected(inputs,
-                                   size,
-                                   scope=scope,
-                                   reuse=reuse)
-        net = slim.dropout(net, dropout_rate,
-                is_training=is_training, scope='dropout')
-        tf.summary.histogram('activations/auxnet/embedding', net)
-    return net
+    def build(self, input_shape):
+        # Create a trainable weight variable for this layer.
+        self.bias = self.add_weight(name='bias',
+                                    shape=(self.output_dim,),
+                                    initializer='zeros',
+                                    trainable=True)
+        super().build(input_shape)  # Be sure to call this somewhere!
 
+    def call(self, x):
+        return K.bias_add(x, self.bias)
 
-def auxnet(embedding, size, dropout_rate=0.5, is_training=True, scope='auxnet'):
-    # MLP in auxiliary networks
-    with tf.variable_scope(scope, 'AuxNet'):
-        with slim.arg_scope([slim.fully_connected],
-                weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                weights_regularizer=slim.l2_regularizer(0.005),
-                activation_fn=tf.nn.relu):
-
-            net = slim.fully_connected(embedding, size, scope='hidden')
-            net = slim.dropout(net, dropout_rate,
-                    is_training=is_training, scope='dropout')
-            net = slim.fully_connected(net, size, scope='output',
-                    activation_fn=None)
-
-    tf.summary.histogram('activations/auxnet/%s' % scope, net)
-    return net
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 
-def diet(inputs, outputs, xt,
+#non-parametric dense layer
+NPDense = lambda name: Lambda(lambda x: K.dot(x[0], x[1]), name=name)
+
+
+# embedding closure for shared parameters
+def Embedding(size, activation='elu', dropout_rate=0.):
+    dense = Dense(size)
+    bn    = BatchNormalization()
+    act   = Activation(activation)
+    drop  = Dropout(dropout_rate)
+    def emb(x):
+        return drop(act(bn(dense(x))))
+    return emb
+
+
+def MLP(inp, size=100, activation='linear', dropout_rate=0.):
+    ret = Dense(size)(inp)
+    ret = BatchNormalization()(ret)
+    ret = Activation(activation)(ret)
+    if dropout_rate > 0.:
+        ret = Dropout(dropout_rate)(ret)
+    return ret
+
+
+def diet(input_size, output_size, xt_size=None,
          batch_size=64,
          hidden_size=100,
          embedding_size=100,
-         dropout_rate=0.5,
-         is_training=True,
+         dropout_rate=0.,
          use_aux=True,
          autoencoder=True,
-         gamma=1,
+         gamma=1.,
          share_embedding=True,
-         scope=None):
+         activation='elu'):
 
-    with slim.arg_scope([slim.fully_connected],
-                      activation_fn=tf.nn.relu):
+    X = Input(shape=(input_size,), name='x')
+    inputs = [X]
 
-        input_size = inputs.get_shape().as_list()[1]
-        output_size = outputs.get_shape().as_list()[1]
+    if use_aux:
+        assert xt_size, "use_aux requires x_transpose matrix"
+        Xt = Input(shape=(xt_size,), name='x_t')
+        inputs += [Xt]
 
-        # use placeholder_with_default hack to be able to run valid set
-        # see: https://github.com/tensorflow/tensorflow/issues/2514
-        inputs  = tf.placeholder_with_default(inputs,  [None,  input_size], name='inputs')
-        outputs = tf.placeholder_with_default(outputs, [None, output_size], name='outputs')
+        # might be shared
+        embed = Embedding(embedding_size, activation, dropout_rate=dropout_rate)
+        embed_xt = embed(Xt)
 
+        We = MLP(embed_xt, hidden_size, 'linear', dropout_rate)
+
+        hidden0 = NPDense(name='fat_hidden')([X, We])
+        # add bias
+        hidden0 = Bias(hidden_size, name='fat_hidden_bias')(hidden0)
+        hidden0 = Activation(activation, name='fat_hidden_act')(hidden0)
+    else:
+        hidden0 = Dense(hidden_size, activation=activation, name='fat_hidden')(X)
+
+    hidden1 = MLP(hidden0, hidden_size, activation, dropout_rate)
+    predictions = MLP(hidden1, size=output_size, activation='softmax')
+    outputs = [predictions]
+    loss = ['categorical_crossentropy']
+    loss_weights = [1.]
+
+    if autoencoder:
         if use_aux:
-            embed = embedding(xt, embedding_size, dropout_rate=dropout_rate,
-                              is_training=is_training, scope='auxembed')
-            We = auxnet(embed, hidden_size, dropout_rate=dropout_rate,
-                        is_training=is_training, scope='aux_We')
+            if not share_embedding:
+                #create new embedding
+                embed = Embedding(embedding_size, dropout_rate=dropout_rate)
+                embed_xt = embed(Xt)
 
+            Wd = MLP(embed_xt, hidden_size, 'linear', dropout_rate)
+            Wd = Lambda(lambda x: K.transpose(x), name='transpose')(Wd)
+
+            xhat = NPDense(name='fat_hidden_ae')([hidden1, Wd])
+            # add bias
+            xhat = Bias(hidden_size, name='fat_hidden_bias_ae')(xhat)
         else:
-            We = slim.model_variable('We',
-                                     shape=(input_size, hidden_size),
-                                     initializer=tf.truncated_normal_initializer(stddev=0.01),
-                                     regularizer=slim.l2_regularizer(0.0005))
+            xhat = Dense(hidden_size, name='fat_hidden_ae')(hidden1)
 
-        We = tf.clip_by_norm(We, clip_norm=1)
+        outputs += [xhat]
+        loss += ['mse']
+        loss_weights += [gamma] #coefficient of AE loss
 
-        tf.summary.histogram('weights/dietnet/We', We)
+    model = Model(inputs=inputs, outputs=outputs)
 
-        output_h1 = tf.matmul(inputs, We)
-        output_h1 = slim.bias_add(output_h1, activation_fn=tf.nn.relu)
-        tf.summary.histogram('activations/dietnet/output_h1', output_h1)
-        output_h1 = slim.dropout(output_h1, dropout_rate, is_training=is_training)
-
-        with tf.variable_scope(scope, 'DietNet'):
-            class_predictions = slim.fully_connected(output_h1, output_size,
-                                                     activation_fn=None,
-                                                     scope='output')
-
-        entropy_loss = slim.losses.softmax_cross_entropy(class_predictions, outputs)
-        tf.summary.scalar('loss/crossent_loss', entropy_loss)
-        tf.summary.scalar('accuracy',
-                slim.metrics.accuracy(tf.argmax(class_predictions, 1),
-                                      tf.argmax(outputs, 1)))
-
-        if autoencoder:
-            if use_aux:
-                if share_embedding:
-                    Wd = auxnet(embed, hidden_size, dropout_rate=dropout_rate,
-                                is_training=is_training, scope='aux_Wd')
-                else:
-                    embed2 = embedding(xt, embedding_size, scope='auxembed2')
-                    Wd = auxnet(embed2, hidden_size,
-                            dropout_rate=dropout_rate,
-                            is_training=is_training, scope='aux_Wd')
-                Wd = tf.transpose(Wd)
-            else:
-                Wd = slim.model_variable('Wd',
-                                         shape=(hidden_size, input_size),
-                                         initializer=tf.truncated_normal_initializer(stddev=0.01),
-                                         regularizer=slim.l2_regularizer(0.0005))
-
-            Wd = tf.clip_by_norm(Wd, clip_norm=1)
-            tf.summary.histogram('weights/dietnet/Wd', Wd)
-
-            xhat = tf.matmul(output_h1, Wd)
-            xhat = slim.bias_add(xhat, activation_fn=tf.nn.relu)
-            mean_squared_loss = slim.losses.mean_squared_error(xhat,
-                                                               inputs,
-                                                               weight=gamma)
-            tf.summary.scalar('loss/autoencoder_mse_loss', mean_squared_loss)
-
-    total_loss = slim.losses.get_total_loss()
-    tf.summary.scalar('loss/total_loss', total_loss)
-
-    return total_loss
+    return model, loss, loss_weights
 
